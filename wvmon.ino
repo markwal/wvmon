6e54df529c81f4f6234242038a8381bd98c5250f
Mark Walker <markwal@hotmail.com>
2023-01-27 14:22:44 -0800

    Fix Adafruit IO connection loops and cmd interp

--- wvmon.ino
+++ wvmon.ino
@@ -10,9 +10,14 @@ Preferences prefs;
 // Adafruit IO
 // ---------------------------------------------------------------------------
 #include "AdafruitIO_WiFi.h"
+String ssid;
+String wifi_pass;
+String io_user;
+String io_key;
 AdafruitIO_WiFi *io = NULL;
 AdafruitIO_Feed *color = NULL;
 AdafruitIO_Feed *temperature = NULL;
+bool aio_failed = false;
 
 // ---------------------------------------------------------------------------
 // Adafruit MCP9808 QT board
@@ -27,6 +32,8 @@ bool sensor_inited = false;
 // Over-The-Air Update
 // Inherits wifi set up from Adafruit IO
 // ---------------------------------------------------------------------------
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 bool arduino_ota_inited = false;
 
@@ -63,6 +70,7 @@ void setup() {
 
   // wait for serial monitor to open
   while(! Serial);
+  Serial.println("Booting...");
 
   // prefs
   prefs.begin(PREFS_NS, false);
@@ -99,15 +107,19 @@ void loop() {
   if (io == NULL) {
     connectIO();
   }
-  else {
-    io->run();
+  else if (!aio_failed) {
+    aio_status_t status = io->run(0, true);
+    if (status == AIO_CONNECT_FAILED || status == AIO_AUTH_FAILED) {
+      aio_failed = true;
+      timer.in(60000, reconnect_aio);
+    }
   }
 
   // check for OTA
   if (!arduino_ota_inited) {
     init_arduino_ota();
     if (!arduino_ota_inited) {
-      // if we're still not initizialized, it's probably because we have no wifi,
+      // if we're still not initialized, it's probably because we have no wifi,
       // let's take a breather
       delay(2000);
     }
@@ -117,44 +129,150 @@ void loop() {
   }
 
   timer.tick();
-  // REVIEW does it matter that we spin hard when the ssid isn't set?
+}
+
+bool reconnect_aio(void *arg)
+{
+  io->connect();
+  aio_failed = false;
+  wait_for_wifi();
+
+  return false;
 }
 
 void connectIO()
 {
   // connect to io.adafruit.com
   if (io == NULL) {
-    String ssid = prefs.getString("ssid");
-    if (ssid.length() == 0) {
-      Serial.println("ssid not sent, skipping wifi");
+    if (prefs.getString("ssid").length() == 0) {
+      Serial.println("ssid not set, skipping wifi");
       return;
     }
 
-    io = new AdafruitIO_WiFi(prefs.getString("io_user").c_str(), prefs.getString("io_key").c_str(), ssid.c_str(), prefs.getString("wifi_pass").c_str());
+    // have to hold these in globals because AdafruitIO just holds onto the pointers we pass to them
+    ssid = prefs.getString("ssid");
+    wifi_pass = prefs.getString("wifi_pass");
+    io_user = prefs.getString("io_user");
+    io_key = prefs.getString("io_key");
+    Serial.printf("ssid: '%s', pwd: '%s'\n", ssid.c_str(), wifi_pass.c_str());
+    Serial.printf("io_user: '%s', io_key: '%s'\n", io_user.c_str(), io_key.c_str());
+    io = new AdafruitIO_WiFi(io_user.c_str(), io_key.c_str(), ssid.c_str(), wifi_pass.c_str());
     if (io == NULL) {
       Serial.println("Unable to create AdafruitIO object");
       return;
     }
     color = io->feed(prefs.getString("io_color", "color").c_str());
     color->onMessage(handleColorMessage);
-    temperature = io->feed(prefs.getString("io_feed", "temp").c_str());
+    temperature = io->feed(prefs.getString("io_feed", "temperature").c_str());
   }
 
-  Serial.print("Connecting to Adafruit IO");
+  Serial.println("Connecting to Adafruit IO");
   io->connect();
 
-  // wait for a connection
-  for(int i = 0; io->status() < AIO_CONNECTED && i < 10; i++) {
-    io->run();
-    Serial.print(".");
-    delay(500);
+  wait_for_wifi();
+  wait_for_aio();
+
+  Serial.println(io->statusText());
+  // if we're still not connected, try scanning wifi, sometimes that kicks it loose
+  if (io->status() < AIO_CONNECTED) {
+    scan_wifi();
+    wait_for_aio();
   }
 
   // if we're connected, fetch the initial pixel color
-  Serial.println(io->statusText());
   if (io->status() >= AIO_CONNECTED) {
     color->get();
   }
+}
+
+void wait_for_aio()
+{
+  // wait for a Adafruit IO connection
+  for (int i = 0; io->status() < AIO_CONNECTED && i < 10; i++) {
+    Serial.print("[AIO] "); Serial.println(io->statusText());
+    if (io->status() == AIO_CONNECT_FAILED) {
+      break;
+    }
+    io->run(0, true);
+    delay(500);
+  }
+  Serial.print("[AIO] "); Serial.println(io->statusText());
+}
+
+void scan_wifi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  Serial.println("WiFi Scan");
+
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0) {
+      Serial.println("no networks found");
+  } else {
+      Serial.print(n);
+      Serial.println(" networks found");
+      for (int i = 0; i < n; ++i) {
+          // Print SSID and RSSI for each network found
+          Serial.print(i + 1);
+          Serial.print(": ");
+          Serial.print(WiFi.SSID(i));
+          Serial.print(" (");
+          Serial.print(WiFi.RSSI(i));
+          Serial.print(")");
+          Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+          delay(10);
+      }
+  }
+
+  // reconnect wifi after scan
+  WiFi.begin(ssid.c_str(), wifi_pass.c_str());
+  wait_for_wifi();
+}
+
+const char *wifi_status_text()
+{
+  switch(WiFi.status()) {
+    case WL_NO_SSID_AVAIL:
+      return "[WiFi] SSID not found";
+    case WL_CONNECT_FAILED:
+      return "[WiFi] Failed - WiFi not connected!";
+    case WL_CONNECTION_LOST:
+      return "[WiFi] Connection was lost";
+    case WL_SCAN_COMPLETED:
+      return "[WiFi] Scan is completed";
+    case WL_DISCONNECTED:
+      return "[WiFi] WiFi is disconnected";
+    case WL_CONNECTED:
+      return "[WiFi] WiFi is connected!";
+    default:
+      Serial.print("[WiFi] Unknown status: ");
+      Serial.println(WiFi.status());
+      return "[WiFi] Unknown status";
+  }
+}
+
+void wait_for_wifi()
+{
+    // Will try for about 10 seconds (20x 500ms)
+    int tryDelay = 500;
+    int numberOfTries = 20;
+
+    while (--numberOfTries >= 0) {
+      Serial.println(wifi_status_text());
+      switch(WiFi.status()) {
+        case WL_CONNECT_FAILED:
+          return;
+        case WL_CONNECTED:
+          Serial.print("[WiFi] IP address: ");
+          Serial.println(WiFi.localIP());
+          return;
+      }
+      delay(tryDelay);
+    }
+    Serial.print("[WiFi] Failed to connect");
 }
 
 void init_arduino_ota()
@@ -273,7 +391,7 @@ void doCommand(char *command) {
   static const command_name_t string_settings[] = {
     COMMAND_NAME("ssid"),
     COMMAND_NAME("wifi_pass"),
-    COMMAND_NAME("io_username"),
+    COMMAND_NAME("io_user"),
     COMMAND_NAME("io_key"),
     COMMAND_NAME("ota_hostname"),
     COMMAND_NAME("ota_hash"),
@@ -281,15 +399,15 @@ void doCommand(char *command) {
     COMMAND_NAME("io_feed")
   };
 
-  for (int i = 0; i < sizeof(string_settings); i++) {
+  for (int i = 0; i < sizeof(string_settings) / sizeof(command_name_t); i++) {
     const command_name_t *setting = &string_settings[i];
-    if (strcasecmp(command, setting->command) == 0) {
-      int l = setting->length;
+    int l = setting->length - 1; // sizeof counts the null terminator
+    if (strncasecmp(command, setting->command, l) == 0) {
       if (command[l] == ' ') {
         prefs.putString(string_settings[i].command, command + l + 1);
         Serial.printf("Stored '%s' in '%s'.\n", command + l + 1, setting->command);
       }
-      else {
+      else if (command[l] == 0) {
         prefs.remove(setting->command);
         Serial.printf("Removed setting '%s'\n", setting->command);
       }
@@ -300,24 +418,27 @@ void doCommand(char *command) {
   // since we only have a few of these odd ones will do it ad-hoc, if we decide
   // later to have a bunch, we should integrate it into the loop above (command descriptor with types)
   const command_name_t clear = COMMAND_NAME("clear");
-  if (strcasecmp(command, clear.command)) {
+  if (strcasecmp(command, clear.command) == 0) {
+    Serial.println("Clearing Preferences.");
     prefs.clear();
     return;
   }
   const command_name_t restart = COMMAND_NAME("restart");
-  if (strcasecmp(command, restart.command)) {
+  if (strcasecmp(command, restart.command) == 0) {
+    Serial.println("Restarting.");
     ESP.restart();
     // not reached
   }
   const command_name_t nvs_reset = COMMAND_NAME("nvs_reset");
-  if (strcasecmp(command, nvs_reset.command)) {
+  if (strcasecmp(command, nvs_reset.command) == 0) {
+    Serial.println("Resetting NVS flash memory.");
     nvs_flash_erase();
     nvs_flash_init();
     ESP.restart();
     // not reached
   }
   const command_name_t ota_port = COMMAND_NAME("ota_port");
-  if (strcasecmp(command, ota_port.command)) {
+  if (strcasecmp(command, ota_port.command) == 0) {
       if (command[ota_port.length] == ' ') {
         int port = atoi(command + ota_port.length + 1);
         prefs.putInt(ota_port.command, port);
@@ -338,6 +459,7 @@ void serialEvent() {
 
   while (Serial.available()) {
     char inChar = (char)Serial.read();
+    Serial.write(inChar);
     if (inChar == '\n' || ich >= INPUT_BUFFER_SIZE - 1) {
       if (ich > INPUT_BUFFER_SIZE - 1)
         ich = INPUT_BUFFER_SIZE - 1;  // should be not reached
